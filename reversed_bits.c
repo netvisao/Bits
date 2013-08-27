77625c9e0259e79bf23b94fc68c58b0bc8ef79c5
Amine Rounak <netvisao@gmail.com>
2013-08-26 23:53:53 -0400

    reversed_bits

--- reversed_bits.c
+++ reversed_bits.c
@@ -1,9 +1,13 @@
+//http://www.geeksforgeeks.org/cisco-interview-set-4/
+#include <stdio.h>
+
+
 unsigned int reverseBits(unsigned int num)
 {
     unsigned int BITS = sizeof(num) * 8;
     unsigned int reverse_num = 0, i;
     
-    for (int i = 0; i < NO_OF_BITS; i++)
+    for (int i = 0; i < BITS; i++)
       reverse_num |= ((2^i & num) << (BITS - 1 - i));
     return reverse_num;
 }
